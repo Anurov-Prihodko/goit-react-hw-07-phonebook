495e8df902bc993e6f34a53b803b7da3f230bc28
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-09-22 17:48:56 +0300

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,9 +7,21 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
-
 import logger from 'redux-logger';
 import contacts from './phonebook/contacts-reducer';
+
+const myMiddleware = store => next => action => {
+  console.log('Моя прослойка ', action);
+
+  next(action);
+};
+
+// function myMiddleware (store) {
+//   return function (next) {
+//     return function (action) {
+//     }
+//   }
+//  }
 
 const middleware = [
   ...getDefaultMiddleware({
@@ -17,6 +29,7 @@ const middleware = [
       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
     },
   }),
+  myMiddleware,
   logger,
 ];
 
